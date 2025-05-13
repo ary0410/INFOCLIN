ff9aa6e54ee157075e517fbd75c7fe4fa2ffdaac
Maria Arielly <mags12@aluno.ifal.edu.br>
2025-05-13 10:38:03 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:infoclin2/home_page.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        home: HomePageInfoclin()
+      )
+
+          //lalala
+  );
+}
