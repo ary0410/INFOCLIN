5edca40d0403fa7ab2512b14abdc63ea71eea8fd
Ary <mags12@aluno.ifal.edu.br>
2025-05-13 10:12:13 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:teste_app/home_page.dart';
+import 'package:teste_app/home_page_infoclin.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        home: HomePageInfoclin(),
+      )
+  );
+}
