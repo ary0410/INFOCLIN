12458d39d7cec19d41fd6363f4d810f7cfb1c332
mklara912 <mkvss1@aluno.ifal.edu.br>
2025-06-10 11:21:02 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:teste_app/home_page.dart';
-import 'package:teste_app/home_page_infoclin.dart';
+import 'dicasExames.dart';
 
 void main() {
   runApp(
-      MaterialApp(
-        home: HomePageInfoclin(),
+      const MaterialApp(
+        home: DicasdeexamesInfoclin(),
       )
   );
 }
+
